8e9b214ecac8735bfdfe1eeb50ee91a598e9fd47
Tommy Lockwood <t.lockwood@erasolutions.us>
2026-01-30 14:00:48 -0600

    Updated Sanity read token

--- src/sanity/config/client-config.ts
+++ src/sanity/config/client-config.ts
@@ -1,10 +1,10 @@
 const config = {
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  title: process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE,
-  dataset: "production",
-  apiVersion: "2023-06-19",
-  token: process.env.NEXT_PUBLIC_SANITY_API_TOKEN, 
-  useCdn: false,
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    title: process.env.NEXT_PUBLIC_SANITY_PROJECT_TITLE,
+    dataset: 'production',
+    apiVersion: '2023-06-19',
+    token: process.env.SANITY_READ_TOKEN,
+    useCdn: false,
 };
 
 export default config;
